117d5c5d4eb4fc90723b7e5af2833154a6171e64
Polina Pavlova <poli.paskaleva@gmail.com>
2021-12-16 13:16:35 +0000

    user repo tests added

--- src/test/java/softuni/restaurant/repository/UserRepositoryTest.java
+++ src/test/java/softuni/restaurant/repository/UserRepositoryTest.java
@@ -1,5 +1,7 @@
 package softuni.restaurant.repository;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -11,6 +13,7 @@ import softuni.restaurant.model.entity.UserEntity;
 import softuni.restaurant.model.entity.enums.RoleEnum;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,48 +22,72 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(value = true)
 class UserRepositoryTest {
+    private UserEntity testUser;
+
     @Autowired
     UserRepository userRepository;
 
 
-    @Test
-    public void createUser() {
-        UserEntity userEntity = new UserEntity();
-        userEntity.setEmail("abv@abv.abv");
-        userEntity.setUsername("abv");
-        userEntity.setPassword("abv");
-        userEntity.setRole(RoleEnum.EMPLOYEE);
+   @BeforeEach
+    public void init() {
+        testUser = new UserEntity()
+        .setEmail("test@test.test")
+        .setUsername("test")
+        .setPassword("test")
+        .setRole(RoleEnum.EMPLOYEE);
 
-        UserEntity save = userRepository.save(userEntity);
+       userRepository.save(testUser);
 
-        assertTrue(save.getId() > 0);
+
 
     }
+
 
     @Test
     public void findAllUsers() {
         List<UserEntity> userEntities = userRepository.findAll();
-        userEntities.forEach(System.out::println);
+        Assertions.assertTrue(userEntities.size()>0);
     }
 
     @Test
     public void findUserById() {
-        UserEntity userEntity = userRepository.findById(1L).get();
-        assertNotNull(userEntity);
+        Long expected = testUser.getId();
+        Long actual = userRepository.findById(testUser.getId()).get().getId();
+        assertEquals(expected, actual);
 
     }
 
     @Test
     public void testUpdateUser() {
-        UserEntity userEntity = userRepository.findById(1L).get();
+        UserEntity userEntity = userRepository.findById(testUser.getId()).get();
         userEntity.setActive(false);
         userRepository.save(userEntity);
+        assertFalse(testUser.isActive());
     }
 
-@Test
-    public void deleteUser(){
-    UserEntity userEntity = userRepository.findById(1L).get();
-    userRepository.delete(userEntity);
+    @Test
+    public void deleteUser() {
+       Long id = testUser.getId();
+       userRepository.delete(testUser);
+       assertTrue(userRepository.findById(id).isEmpty());
 
-}
+
+    }
+
+    @Test
+    public void findByUsernameTest() {
+        Optional<UserEntity> byUsername = userRepository.findByUsername(testUser.getUsername());
+        Long actual = byUsername.get().getId();
+
+        assertEquals(testUser.getId(), actual);
+
+    }
+
+    @Test
+    public void findByUsernameIgnoreCaseTest(){
+        String toRandomCase = testUser.getUsername().substring(0, 1).toUpperCase(Locale.ROOT) + testUser.getUsername().substring(1);
+        assertEquals(userRepository.findByUsernameIgnoreCase(testUser.getUsername()).get().getId(),
+                userRepository.findByUsernameIgnoreCase(toRandomCase).get().getId());
+    }
+
 }
