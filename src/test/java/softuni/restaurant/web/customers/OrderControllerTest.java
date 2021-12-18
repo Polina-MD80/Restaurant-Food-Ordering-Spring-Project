25f9fb396627c492e2bffaaef2a16fb0b9440274
Polina Pavlova <poli.paskaleva@gmail.com>
2021-12-18 08:24:36 +0000

    login test added

---
+++ src/test/java/softuni/restaurant/web/customers/OrderControllerTest.java
@@ -0,0 +1,30 @@
+package softuni.restaurant.web.customers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void order() {
+    }
+
+    @Test
+    void testOrder() {
+    }
+
+    @Test
+    void testOrder1() {
+    }
+}
---
+++ src/test/java/softuni/restaurant/web/customers/UserLoginControllerTest.java
@@ -0,0 +1,104 @@
+package softuni.restaurant.web.customers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+
+import softuni.restaurant.model.entity.UserEntity;
+import softuni.restaurant.model.entity.enums.RoleEnum;
+import softuni.restaurant.repository.UserRepository;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserLoginControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private UserEntity testUser;
+
+    @BeforeEach
+    void setUp() {
+        testUser = initTestUser();
+    }
+
+    @AfterEach
+    void tearDown() {
+        userRepository.delete(testUser);
+
+    }
+
+    @Test
+    void loginPageOpens() throws Exception {
+        mockMvc
+                .perform(get("/users/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+    @Test
+    void testLoginCorrectUser() throws Exception {
+        mockMvc
+                .perform(post("/users/login")
+                        .param("username", "test")
+                        .param("password", "test")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(authenticated())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+
+    }
+
+    @Test
+    void testLoginIncorrectUserPass() throws Exception {
+        mockMvc
+                .perform(post("/users/login")
+                        .param("username", "test")
+                        .param("password", "wrong")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(forwardedUrl("/users/login-error"));
+
+    }
+
+    @Test
+    void testLoginIncorrectUsername() throws Exception {
+        mockMvc
+                .perform(post("/users/login")
+                        .param("username", "wrong")
+                        .param("password", "test")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(forwardedUrl("/users/login-error"));
+
+    }
+
+    @Test
+    void failedLogin() {
+
+    }
+
+    private UserEntity initTestUser() {
+        return userRepository.save(new UserEntity()
+                .setUsername("test")
+                .setEmail("test@test.bg")
+                .setPassword(passwordEncoder.encode("test"))
+                .setRole(RoleEnum.ADMIN));
+    }
+
+}
