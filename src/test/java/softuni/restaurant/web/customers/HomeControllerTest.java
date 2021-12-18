de43bdbe75023900594bfb6362e01e115be0d432
Polina Pavlova <poli.paskaleva@gmail.com>
2021-12-18 14:03:26 +0000

    index test

--- src/test/java/softuni/restaurant/web/customers/HomeControllerTest.java
+++ src/test/java/softuni/restaurant/web/customers/HomeControllerTest.java
@@ -1,30 +1,28 @@
 package softuni.restaurant.web.customers;
 
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class TermsControllerTest {
-
+class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
     @Test
     void termsPageOpens() throws Exception {
         mockMvc
-                .perform(get("/terms-and-conditions"))
+                .perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("terms-and-conditions"));
+                .andExpect(view().name("index"))
+                .andExpect(model().attributeExists("allItems"));
     }
-
 
 }
