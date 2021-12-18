e37fc3303956783f593d41b0d0c98f5f3fa91adc
Polina Pavlova <poli.paskaleva@gmail.com>
2021-12-18 08:26:21 +0000

    login test added

---
+++ src/test/java/softuni/restaurant/web/customers/UserRegistrationControllerTest.java
@@ -0,0 +1,25 @@
+package softuni.restaurant.web.customers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRegistrationControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void userModel() {
+    }
+
+    @Test
+    void registerUser() {
+    }
+
+    @Test
+    void register() {
+    }
+}
