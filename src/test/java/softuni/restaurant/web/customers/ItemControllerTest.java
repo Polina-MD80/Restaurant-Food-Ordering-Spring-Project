e5672ae54490c189e9504294c62525f63b13fa5b
Polina Pavlova <poli.paskaleva@gmail.com>
2021-12-18 14:17:38 +0000

    Item test

---
+++ src/test/java/softuni/restaurant/web/customers/ItemControllerTest.java
@@ -0,0 +1,60 @@
+package softuni.restaurant.web.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+@SpringBootTest
+@AutoConfigureMockMvc
+class ItemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void foodsPageOpens() throws Exception {
+        mockMvc
+                .perform(get("/items/foods"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("foods"))
+                .andExpect(model().attributeExists("itemsByType"));
+    }
+
+
+    @Test
+    void drinksPageOpens() throws Exception {
+        mockMvc
+                .perform(get("/items/drinks"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("foods"))
+                .andExpect(model().attributeExists("itemsByType"));
+    }
+
+
+
+    @Test
+    void othersPageOpens() throws Exception {
+        mockMvc
+                .perform(get("/items/others"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("foods"))
+                .andExpect(model().attributeExists("itemsByType"));
+    }
+
+
+
+    @Test
+    void searchPageOpensCorrectlyAfterSearch() throws Exception {
+        mockMvc
+                .perform(get("/items/search").param("keyword", "soup"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("foods"))
+                .andExpect(model().attributeExists("itemsByType"));
+    }
+
+}
